4a2853d2f3d30fb4121050dbb04532a6c03698a7
RubyPiec <kittenboekjes@gmail.com>
2020-02-28 21:06:16 +0100

    Update V0.5

--- challenges.cpp
+++ challenges.cpp
@@ -5,20 +5,18 @@
 #include <cmath>
 
 namespace eggdogg{
-    int rand(min, max){
+    int rand(int min, int max){
       return std::rand()%(max-min)+(min);
     }
 }
 
 int main(){
-    int seed;
-    int string;
     int length;
     std::cout << "Length of rememberance please\n";
     std::cin >> length;
-    std::stoi(length)
-    std::srand(std::time(NULL));
+    std::srand(std::time(NULL)*6942069420);
     int string = eggdogg::rand(pow(10,length-1), (pow(10,length)-1));
+    std::cout << "test";
     std::cout << string;
 }
 
