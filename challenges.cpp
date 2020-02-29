64c12db3a92b9efc117c024ee14bf79a0c66c126
RubyPiec <kittenboekjes@gmail.com>
2020-02-29 10:33:38 +0100

    Version 1.0

--- challenges.cpp
+++ challenges.cpp
@@ -1,6 +1,8 @@
-#include <cstdlib>
+#include <windows.h>
 #include <iostream>
+#include <cstdlib>
 #include <random>
+#include <chrono>
 #include <ctime>
 #include <cmath>
 
@@ -12,11 +14,21 @@ namespace eggdogg{
 
 int main(){
     int length;
+    int answer;
     std::cout << "Length of rememberance please\n";
     std::cin >> length;
     std::srand(std::time(NULL)*6942069420);
     int string = eggdogg::rand(pow(10,length-1), (pow(10,length)-1));
-    std::cout << "test";
     std::cout << string;
+    Sleep(500*length);
+    system("CLS");
+    std::cout << "Please retype the digits that you just saw:\n";
+    std::cin >> answer;
+    if(answer == string){
+      std::cout << "Congratulations, you have it fully correct!";
+    }
+    else{
+      std::cout << "Wrong!\nThe answer was: " << string << "\nYou answered: " << answer;
+    }
 }
 
